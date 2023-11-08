265add1a629e02744978d4ffaa7c1b42a4721f87
Milan5002 <milan.sana@kalvium.community>
2023-11-08 17:17:41 +0530

    done

--- result.js
+++ result.js
@@ -2,9 +2,21 @@ let score_box=document.getElementById("score")
 let play_btn=document.getElementById("play-again")
 let home_btn=document.getElementById("home_return")
 const bgm = new Audio('images/game end.mp3')
+const phrase = document.getElementById('phrases')
 
 let value = localStorage.getItem("score")
 score_box.textContent = value;
+
+var Phrases = [
+  "Amazing!!",
+  "Well played!",
+  "Eureka!!!",
+  "Bull's Eye!!",
+  "Well done !!",
+  "Congrats!!",
+]
+
+phrase.innerText =Phrases[Math.floor(Math.random() * 6)]
 
 home_btn.onclick = () => {
   location.href="./index.html";
